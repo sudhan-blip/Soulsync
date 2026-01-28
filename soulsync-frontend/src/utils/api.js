03c16172b93c7f5ea5c3ddc486a11565ca181c2c
Sudhan <keerthivasanm644@gmail.com>
2026-01-28 14:30:37 +0530

    Update API to use environment variable for deployment

--- soulsync-frontend/src/utils/api.js
+++ soulsync-frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000";
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 function getToken() {
   return localStorage.getItem("token");
